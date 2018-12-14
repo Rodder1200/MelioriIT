a81a0cf36c4d166e09c36653fa76f2544453eeef
Rodder1200 <denegnet@mail.com>
2018-12-14 18:44:39 +0200

    init

---
+++ gulpfile.js
@@ -0,0 +1,123 @@
+var gulp = require("gulp");
+var browserSync = require("browser-sync").create();
+var pug = require("gulp-pug");
+var sass = require("gulp-sass");
+var del = require("del");
+var rename = require("gulp-rename");
+var uglify = require("gulp-uglify");
+var concat = require("gulp-concat");
+var srcmaps = require("gulp-sourcemaps");
+
+var imagemin = require("gulp-imagemin");
+var imageminJpegRecompress = require("imagemin-jpeg-recompress");
+var pngquant = require("imagemin-pngquant");
+var cache = require("gulp-cache");
+
+/*---------Server---------*/
+gulp.task("server", function() {
+  browserSync.init({
+    server: {
+      baseDir: "docs/"
+    }
+  });
+
+  gulp.watch("docs/**/*").on("change", browserSync.reload);
+});
+
+/*---------Pug-compile---------*/
+gulp.task("templates:compile", function buildHTML() {
+  return gulp
+    .src("src/template/index.pug")
+    .pipe(
+      pug({
+        pretty: true
+      })
+    )
+    .pipe(gulp.dest("docs/"));
+});
+
+/*---------Styles-compile---------*/
+gulp.task("styles:compile", function() {
+  return gulp
+    .src("src/styles/main.scss")
+    .pipe(sass({ outputStyle: "compressed" }).on("error", sass.logError))
+    .pipe(rename("main.min.css"))
+    .pipe(gulp.dest("docs/css"));
+});
+
+/* ------------ JS ------------- */
+gulp.task("js", function() {
+  return gulp
+    .src([
+      // "src/js/_init.js",
+      "src/js/index.js"
+    ])
+    .pipe(srcmaps.init())
+    .pipe(concat("main.min.js"))
+    .pipe(uglify())
+    .pipe(srcmaps.write())
+    .pipe(gulp.dest("docs/js"));
+});
+
+/* ------------ Delete ------------- */
+gulp.task("clean", function() {
+  return del(["build"]);
+});
+
+/* ------------ Copy fonts ------------- */
+gulp.task("copy:fonts", function() {
+  return gulp.src("./src/fonts/**/*.*").pipe(gulp.dest("docs/fonts"));
+});
+
+/* ------------ Copy images ------------- */
+gulp.task("copy:images", function() {
+  return gulp
+    .src("./src/imgs/**/*.*")
+    .pipe(
+      cache(
+        imagemin(
+          [
+            imagemin.gifsicle({ interlaced: true }),
+            imagemin.jpegtran({ progressive: true }),
+            imageminJpegRecompress({
+              loops: 5,
+              min: 65,
+              max: 70,
+              quality: "medium"
+            }),
+            imagemin.svgo(),
+            imagemin.optipng({ optimizationLevel: 3 }),
+            pngquant({ quality: "65-70", speed: 5 })
+          ],
+          {
+            verbose: true
+          }
+        )
+      )
+    )
+    .pipe(gulp.dest("docs/imgs"));
+});
+
+// Clearing the cache
+gulp.task("clear", function(done) {
+  return cache.clearAll(done);
+});
+
+/* ------------ Copy ------------- */
+gulp.task("copy", gulp.parallel("copy:fonts", "copy:images"));
+
+/* ------------ Watchers ------------- */
+gulp.task("watch", function() {
+  gulp.watch("src/template/**/*.pug", gulp.series("templates:compile"));
+  gulp.watch("src/styles/**/*.scss", gulp.series("styles:compile"));
+  gulp.watch("src/js/**/*.js", gulp.series("js"));
+});
+
+gulp.task(
+  "default",
+  gulp.series(
+    "clean",
+    gulp.parallel("templates:compile", "styles:compile", "js", "copy"),
+    gulp.parallel("watch", "server")
+  )
+);
